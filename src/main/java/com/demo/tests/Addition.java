4b6c582455b11b22cca4e97cfec304dc76a2fba7
Anveshak <Anveshak@192.168.1.51>
2023-11-30 10:21:19 +0530

    First commit All file changes

---
+++ src/main/java/com/demo/tests/Addition.java
@@ -0,0 +1,12 @@
+package com.demo.tests;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int x= 10;
+		int y = 20;
+		int add = 10+20;
+		System.out.println("Addition of two numbers is:"+ add);
+	}
+	
+}
